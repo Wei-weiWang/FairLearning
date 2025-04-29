196b3e0e36d37e493d925b0232df8685ff78cc03
Wei-weiWang <67363436+Wei-weiWang@users.noreply.github.com>
2025-04-29 01:24:11 -0500

    Update fairfunctions.R

--- fairfunctions.R
+++ fairfunctions.R
@@ -1,32 +1,15 @@
-# Solver for gpava
-poisson_solver = function(Y, w){ 
-  
-  return(   sum(Y*w)/sum(w)     )  
-  
-}
-
-poissonloss = function( Y, expfhat  ){ 
-  
-  ans <- ifelse(Y == 0, expfhat, expfhat - Y* log(expfhat )  )
-  return( mean (ans) ) 
-  
-}
 
 quantile_loss <- function(y, y_hat, tau) {
-  loss <- numeric(length(y))
-  for (i in seq_along(y)) {
-    if (y[i] < y_hat[i]) {
-      loss[i] <- (tau - 1) * (y[i] - y_hat[i])
-    } else {
-      loss[i] <- tau * (y[i] - y_hat[i])
-    }
-  }
-  return(mean(loss))
+  loss <- ifelse(y < y_hat, (tau - 1) * (y - y_hat), tau * (y - y_hat))
+  mean(loss)
 }
+# Test: 
+# 1. This function is aligned with definition in https://www.lokad.com/pinball-loss-function-definition/
+# 2.quantile_loss(c(1,2,3,4), c(0.3,1,2,4),0.9) > quantile_loss(c(1,2,3,4), c(5,6,7,8),0.9)
 
+# KS statistic
 f_ks <- function(Y, S) {
-  # Generate the sequence of thresholds
-  tt <- seq(min(Y), max(Y), length.out = 2000)
+  ts <- sort(unique(Y))
   
   # Get unique classes
   classes <- unique(S)
@@ -47,7 +30,7 @@ f_ks <- function(Y, S) {
       
       # Compute KS statistic for this pair of classes
       fai <- 0
-      for (t in tt) {
+      for (t in ts) {
         tmp1 <- sum(Y[S == class_i] <= t) / nn_i
         tmp2 <- sum(Y[S == class_j] <= t) / nn_j
         fai <- max(fai, abs(tmp1 - tmp2))
@@ -60,223 +43,72 @@ f_ks <- function(Y, S) {
   
   return(max_fai)
 }
+# Test: 
+# 1.definition of each line
+# 2.examples: f_ks(1:5, c(1,1,1,2,2)) = 1 f_ks(1:5, c(1,2,1,2,1)) = 0.3333333
+# f_ks(1:10, c(1,2,3,3,2,1,1,2,3,2)) = 0.5 f_ks(1:10, c(1,1,1,2,2,2,3,3,3)) = 1
+
+#------------------------------------
+# ISPLINE METHOD FUNCTIONS
+#------------------------------------
+
+# https://www.fon.hum.uva.nl/praat/manual/spline.html Mspline is decided by t. t is decided by order and k-sigh_i
+# From R package: if both knots and Boundary.knots are supplied, the basis parameters do not depend on x
+fit_ispline_model <- function(rank_train, Y_train, tau = 0.75, 
+                              knots = c(0.2, 0.5, 0.75),  degree = 2) {
+  is_basis <- iSpline(rank_train, knots = knots, Boundary.knots = c(0,1),  degree = degree) # Generate iSpline 
+  
+  quantile_loss_objective <- function(par) {
+    y_pred <- cbind(is_basis, 1) %*% par  # add intercept to the last column
+    quantile_loss(Y_train, y_pred, tau) # Calculate the loss
+  }
+  initial_par <- rep(1, ncol(is_basis) + 1) # Initialize parameter
+  optim_result <- hjkb(initial_par, quantile_loss_objective, 
+                       lower = c(rep(0, ncol(is_basis)), -Inf))# Update the parameter to get the best one
+  list(coeff = optim_result$par)
+}
+
+# Test: is_basis is plotted, which looks good.
+
+# Given a (possibly new) vector of ranks, predict responses using the fitted Ispline model.
+ispline_predict <- function(rank_new, coeff, 
+                            knots = c(0.2, 0.5, 0.75), degree = 2) {
+  new_basis <- iSpline(rank_new, knots = knots,  Boundary.knots = c(0,1), degree = degree)
+  as.vector(cbind(new_basis, 1) %*% coeff) # To do the prediction
+}
 
 
-# Note that the training and test data should have been rearranged according to sensitive variable, respectively, before using this Ifairpoi, Ifairqua and Pfair.
-# Here, we require that the input vectors Strain and Stest are sorted in increasing order (e.g., c(0, 0, 1, 1, 1)).
-Ifairpoi = function(fLYpredictions, fTYpredictions, fStrain, fStest, fYtrain){
+#------------------------------------
+# PAVA METHOD FUNCTIONS
+#------------------------------------
+
+fit_pava_model <- function(rank_train, Y_train, tau = 0.75, solverinput = weighted.fractile) {
+  gpava_result <- gpava(rank_train, Y_train, 
+                        solver = solverinput, ties = "secondary", p = tau)
+  fitted_values <- gpava_result$x
   
-  YLF0 = fLYpredictions[which(fStrain==0)]
-  rankYLF0 = rank(YLF0, ties.method = "max")/length(YLF0)
-  YLF1 = fLYpredictions[which(fStrain==1)]
-  rankYLF1 = rank(YLF1, ties.method = "max")/length(YLF1)
+  # Remove duplicates based on training ranks
+  unique_idx <- !duplicated(rank_train)
+  rank_unique <- rank_train[unique_idx]
+  gpava_unique <- fitted_values[unique_idx]
   
-  YLT01 = fYtrain#YLT list
-  rankYLF01 = c(rankYLF0, rankYLF1)# Rank of each element in YLT list(decided by YLF)
+  # Order the unique values
+  ord <- order(rank_unique)
+  rank_unique <- rank_unique[ord]
+  gpava_unique <- gpava_unique[ord]
   
-  isknots = c(0.2,0.5,0.75)
-  degree = 2
-  
-  
-  is_basis <- iSpline(rankYLF01, knots = isknots, degree = degree)
-  
-  poisson_loss_tie <- function(par) {
-    
-    
-    column_of_ones <- rep(1, nrow(is_basis))
-    
-    # Add the column of 1s to the matrix
-    is_basis_wiin <- cbind(is_basis, column_of_ones)
-    
-    # Calculate the predicted y values
-    y_pred = is_basis_wiin%*%par
-    
-    # Calculate Pinball loss
-    Ploss = poissonloss( YLT01, exp(y_pred))
-    
-    return(Ploss)
-  }
-  
-  initial_par <- rep(1, ncol(is_basis)+1 )
-  
-  optim_result = hjkb(initial_par, poisson_loss_tie, lower = c(rep(0, ncol(is_basis)) ,-Inf) )
-  
-  # Predict on the train data using the optimal coefficients
-  column_of_ones <- rep(1, nrow(is_basis))
-  train_pred <- cbind(is_basis, column_of_ones) %*% optim_result$par
-  
-  
-  # Predict on the test data
-  YTF0 = fTYpredictions[which(fStest==0)]
-  nYLF0 = length(YLF0)
-  rankYTF0 <- sapply(YTF0, function(x) sum(YLF0 <= x)) / nYLF0
-  
-  YTF1 = fTYpredictions[which(fStest==1)]
-  nYLF1 = length(YLF1)
-  rankYTF1 <- sapply(YTF1, function(x) sum(YLF1 <= x)) / nYLF1
-  
-  
-  rankYTF01 = c(rankYTF0, rankYTF1)
-  
-  i_spline_basis_test <- iSpline(rankYTF01, knots = isknots, degree = degree)
-  column_of_ones <- rep(1, nrow(i_spline_basis_test))
-  test_pred <- cbind(i_spline_basis_test, column_of_ones) %*% optim_result$par
-  
-  return(list(fairtrain = exp(train_pred), fairtest = exp(test_pred)))
-  
-  
-  
+  # Determine knot positions: indices where the fitted values change
+  knots_idx <- which(diff(gpava_unique) != 0) + 1
+  y0 <- c(gpava_unique[1], gpava_unique[knots_idx])
+  # Build a step function; here we use the index positions as knots.
+  model_fun <- stepfun(knots_idx, y0)
+  list(rank_unique = rank_unique, model = model_fun)
 }
-Pfair = function(fLYpredictions, fTYpredictions, fStrain, fStest, fYtrain, method = poisson_solver, quantile = NA){
-  
-  YLF0 = fLYpredictions[which(fStrain==0)]
-  rankYLF0 = rank(YLF0, ties.method = "max")/length(YLF0)
-  YLF1 = fLYpredictions[which(fStrain==1)]
-  rankYLF1 = rank(YLF1, ties.method = "max")/length(YLF1)
-  
-  YLT01 = fYtrain#YLT list
-  rankYLF01 = c(rankYLF0, rankYLF1)# Rank of each element in YLT list(decided by YLF)
-  
-  
-  # predictions of train and test data by pava
-  gpavapre = gpava(rankYLF01, YLT01, solver = method, ties = "secondary", p =quantile)
-  gpavapre = gpavapre$x
-  
-  
-  # Predict on the test data
-  YTF0 = fTYpredictions[which(fStest==0)]
-  nYLF0 = length(YLF0)
-  rankYTF0 <- sapply(YTF0, function(x) sum(YLF0 <= x)) / nYLF0
-  
-  YTF1 = fTYpredictions[which(fStest==1)]
-  nYLF1 = length(YLF1)
-  rankYTF1 <- sapply(YTF1, function(x) sum(YLF1 <= x)) / nYLF1
-  rankYTF01 = c(rankYTF0, rankYTF1)
-  
-  
-  
-  # For test data to use PAVA
-  gpavapreuni = c()
-  rankYLF01uni = c()
-  
-  for (i in 1:length(rankYLF01)) {
-    if(rankYLF01[i] %in% rankYLF01uni == TRUE){
-      next
-      
-    }else{
-      
-      rankYLF01uni = c(rankYLF01uni, rankYLF01[i])
-      gpavapreuni = c(gpavapreuni, gpavapre[i])
-      
-    }
-    
-  }
-  orderrankYLF01uni = order(rankYLF01uni)
-  rankYLF01uniordered = rankYLF01uni[orderrankYLF01uni]
-  gpavapreuniordered = gpavapreuni[orderrankYLF01uni]
-  
-  
-  knots = c()
-  
-  for (i in 2:length(rankYLF01uni)) {
-    if(gpavapreuniordered[i]==gpavapreuniordered[i-1]){
-      
-      
-    }else{
-      
-      knots = c(knots, i)
-    }
-    
-  }
-  
-  if(gpavapreuniordered[1]!=0){
-    y0 <- c(gpavapreuniordered[1], gpavapreuniordered[knots])
-  }else{
-    
-    y0 <- c(gpavapreuniordered[knots[1]], gpavapreuniordered[knots])
-    
-  }
-  
-  
-  pavatrainedYhatfun <- stepfun(knots, y0)
-  
-  
-  
-  YT0byPAVA = c()
-  for (i in 1:length(YTF0)) {
-    YT0byPAVA[i] = pavatrainedYhatfun( sum( rankYTF0[i]>= rankYLF01uni) ) }
-  
-  YT1byPAVA = c()
-  for (i in 1:length(YTF1)) {
-    YT1byPAVA[i] = pavatrainedYhatfun( sum( rankYTF1[i]>= rankYLF01uni) ) }
-  
-  
-  # Combine the results
-  YtesthatPAVA = c(YT0byPAVA, YT1byPAVA)
-  
-  
-  return(list(fairtrain = gpavapre, fairtest = YtesthatPAVA ))
-}
-Ifairqua = function(fLYpredictions, fTYpredictions, fStrain, fStest, fYtrain){
-  
-  YLF0 = fLYpredictions[which(fStrain==0)]
-  rankYLF0 = rank(YLF0, ties.method = "max")/length(YLF0)
-  YLF1 = fLYpredictions[which(fStrain==1)]
-  rankYLF1 = rank(YLF1, ties.method = "max")/length(YLF1)
-  
-  YLT01 = fYtrain#YLT list
-  rankYLF01 = c(rankYLF0, rankYLF1)# Rank of each element in YLT list(decided by YLF)
-  
-  isknots = c(0.2,0.5,0.75)
-  degree = 2
-  
-  
-  is_basis <- iSpline(rankYLF01, knots = isknots, degree = degree)
-  
-  quantile_loss_tie <- function(par) {
-    
-    
-    column_of_ones <- rep(1, nrow(is_basis))
-    
-    # Add the column of 1s to the matrix
-    is_basis_wiin <- cbind(is_basis, column_of_ones)
-    
-    # Calculate the predicted y values
-    y_pred = is_basis_wiin%*%par
-    
-    # Calculate Pinball loss
-    Ploss = quantile_loss( YLT01, y_pred, qu)*length(y_pred)
-    
-    return(Ploss)
-  }
-  
-  initial_par <- rep(1, ncol(is_basis)+1 )
-  
-  optim_result = hjkb(initial_par, quantile_loss_tie, lower = c(rep(0, ncol(is_basis)) ,-Inf) )
-  
-  # Predict on the train data using the optimal coefficients
-  column_of_ones <- rep(1, nrow(is_basis))
-  train_pred <- cbind(is_basis, column_of_ones) %*% optim_result$par
-  
-  
-  # Predict on the test data
-  YTF0 = fTYpredictions[which(fStest==0)]
-  nYLF0 = length(YLF0)
-  rankYTF0 <- sapply(YTF0, function(x) sum(YLF0 <= x)) / nYLF0
-  
-  YTF1 = fTYpredictions[which(fStest==1)]
-  nYLF1 = length(YLF1)
-  rankYTF1 <- sapply(YTF1, function(x) sum(YLF1 <= x)) / nYLF1
-  
-  
-  rankYTF01 = c(rankYTF0, rankYTF1)
-  
-  i_spline_basis_test <- iSpline(rankYTF01, knots = isknots, degree = degree)
-  column_of_ones <- rep(1, nrow(i_spline_basis_test))
-  test_pred <- cbind(i_spline_basis_test, column_of_ones) %*% optim_result$par
-  
-  return(list(fairtrain = train_pred, fairtest = test_pred))
-  
-  
-  
+
+# Predict new values from the fitted PAVA model.
+pava_predict <- function(pava_model, rank_new) {
+  sapply(rank_new, function(x) {
+    idx <- sum(x >= pava_model$rank_unique)
+    pava_model$model(idx)
+  })
 }
