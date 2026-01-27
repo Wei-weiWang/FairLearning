eb5e3d64d17d91d399bd90b00bce93b5082a524b
Wei-weiWang <67363436+Wei-weiWang@users.noreply.github.com>
2026-01-26 23:43:59 -0600

    Update FairPoissonHRS.R

--- FairPoissonHRS.R
+++ FairPoissonHRS.R
@@ -1,33 +1,12 @@
-hrs = as.data.frame(health.retirement)#https://search.r-project.org/CRAN/refmans/fairml/html/health.retirement.html
-hrs <- na.omit(hrs) 
-
-# Categorical variables
-nummar = ifelse(hrs$marriage == "Married/Partner", 1, 2)
-hrs$marriage = factor(nummar)
-
-numgender = ifelse(hrs$gender == 'Female', 1, 2)
-hrs$gender = factor(numgender)
-
-hrs$race.ethnicity =  factor(hrs$race.ethnicity, levels = c('NHW','NHB','Hispanic','Other'), labels = 1:4)
-hrs$race =  factor(hrs$race, levels = c('Black','White','Other'), labels = 1:3)
-
-# Change column positions
-cols <- names(hrs)
-# find their positions
-iA <- which(cols == "score")
-iB <- which(cols == "race")
-iC <- which(cols == "race.ethnicity")
-iD <- which(cols == "marriage")
-# swap them
-cols[c(iA, iB, iC, iD)] <- cols[c(iB, iA, iD, iC)]
-
-# reindex the df
-hrs <- hrs[ , cols]
-
+library(quantreg)
+library(splines2)  
+library(dfoptim)
+library(Iso)   
+library(isotone)
+library(rqPen) 
 
 set.seed(50)
-# Pre-allocate result vectors (30 iterations)
-n_iter <- 30
+n_iter <- 100
 PR_PL_T <- numeric(n_iter)
 PR_KS_T <- numeric(n_iter)
 PR_PL_L <- numeric(n_iter)
@@ -44,41 +23,64 @@ PRPA_PL_L <- numeric(n_iter)
 PRPA_KS_L <- numeric(n_iter)
 
 
-sample_size <- floor(0.8 * nrow(hrs))  # 80% training, 20% test
+read_appended_csv <- function(path) {
+  lines <- readLines(path)
+  header <- lines[1]
+  lines_clean <- c(header, lines[lines != header])  # drop repeated headers
+  read.csv(text = paste(lines_clean, collapse = "\n"))
+}
+
+
+# read the data files
+train_all <- read_appended_csv("100hrstrain75.csv")
+test_all  <- read_appended_csv("100hrstest75.csv")
+
+
+n_train_each <- nrow(train_all) / n_iter
+n_test_each  <- nrow(test_all)  / n_iter
+
+# split into a list of 100 data frames
+train_list <- split(train_all, rep(seq_len(n_iter), each = n_train_each))
+test_list  <- split(test_all,  rep(seq_len(n_iter), each = n_test_each))
+
 
 for (k in 1:n_iter) {
   cat("Iteration:", k, "\n")
-
-  train_indices <- sample(seq_len(nrow(hrs)), size = sample_size)
-  train_data <- hrs[train_indices, ]
-  test_data  <- hrs[-train_indices, ] 
+  
+  
+  train_data <- train_list[[k]]
+  test_data  <- test_list[[k]]
+  
   
   idx_cov = 25
   idx_y = 26
   idx_sensi = 27
   
+  train_data$marriage <- factor(train_data$marriage)
+  test_data$marriage <- factor(test_data$marriage)
   
-  #--------------------------
+
   # Plain Poisson Regression
-  #--------------------------
   
-  poisson_model <- glm(hrs$score[train_indices] ~ ., data =  hrs[train_indices, c(-idx_y)], family = poisson())
-  
-  
-  PR_PL_L[k] = poissonloss(hrs$score[train_indices], poisson_model$fitted.values  )
-  PR_KS_L[k] = f_ks(poisson_model$fitted.values, hrs[train_indices, idx_sensi])
+  poisson_model <- glm(train_data$score ~ ., data =  train_data[, c(-idx_y)], family = poisson())
   
   
-  predicted_y <- predict(poisson_model, newdata =  hrs[-train_indices, c(-idx_y)], type = 'response')
-  PR_PL_T[k] = poissonloss(hrs$score[-train_indices], predicted_y )
-  PR_KS_T[k] =  f_ks(predicted_y, hrs[-train_indices, idx_sensi])
+  PR_PL_L[k] = poissonloss(train_data$score, poisson_model$fitted.values  )
+  PR_KS_L[k] = f_ks(poisson_model$fitted.values, train_data[, idx_sensi])
   
   
+  predicted_y <- predict(poisson_model, newdata =  test_data[, c(-idx_y)], type = 'response')
+  PR_PL_T[k] = poissonloss(test_data$score, predicted_y )
+  PR_KS_T[k] =  f_ks(predicted_y, test_data[, idx_sensi])
   
-  # Fair regression methods
-  split_by_gender_train <- split(train_data, train_data[,idx_sensi])
   
-  split_by_gender_test <- split(test_data, test_data[,idx_sensi])
+  train_fit = cbind(train_data, yfit = poisson_model$fitted.values)
+  test_fit = cbind(test_data, yfit = predicted_y)
+  idx_yfit = ncol(train_fit)
+  
+  split_by_sensi_train <- split(train_fit, train_fit[, idx_sensi]) 
+  
+  split_by_sensi_test <- split(test_fit, test_fit[, idx_sensi])
   
   # For train
   Ytrain = c()
@@ -92,23 +94,21 @@ for (k in 1:n_iter) {
   Stest = c()
   
   rankYTF = c()
+  racenum = c()
   
-  for (i in 1:length(unique(train_data[,idx_sensi])) ) {
-    train_cov_split = split_by_gender_train[[i]][1:idx_cov]
-    Ytrain_split = split_by_gender_train[[i]][idx_y]
+  for (i in 1:length(unique(train_fit[, idx_sensi])) ) {
+    LYpred_split = split_by_sensi_train[[i]][idx_yfit][[1]] #note: get columns
+    Ytrain_split = split_by_sensi_train[[i]][idx_y]
+    racenum[i] = nrow(Ytrain_split)
     Ytrain = c(Ytrain, Ytrain_split[[1]])
-    Strain = c(Strain, rep(i, nrow(train_cov_split)))
+    Strain = c(Strain, rep(i, nrow(Ytrain_split)))
     
     
-    test_cov_split = split_by_gender_test[[i]][1:idx_cov]
-    Ytest_split = split_by_gender_test[[i]][idx_y]
+    TYpred_split = split_by_sensi_test[[i]][idx_yfit][[1]]
+    Ytest_split = split_by_sensi_test[[i]][idx_y]
     Ytest = c(Ytest, Ytest_split[[1]])
-    Stest = c(Stest, rep(i, nrow(test_cov_split)))
-    
+    Stest = c(Stest, rep(i, nrow(Ytest_split)))
    
-    model = glm(Ytrain_split[[1]] ~ ., data = train_cov_split,  family = poisson())
-    LYpred_split = predict(model, train_cov_split)
-    TYpred_split = predict(model, test_cov_split)
     
     LYpred = c(LYpred, LYpred_split)
     
@@ -117,71 +117,151 @@ for (k in 1:n_iter) {
     
     rankYTF_split = sapply(TYpred_split, function(y) sum(LYpred_split <= y)/length(LYpred_split))
     rankYTF = c(rankYTF, rankYTF_split)
-    
-    
   }
+  
+ 
+    
+  
   names(rankYLF) <- NULL
   
-  #--------------------------
+
   # Ispline Method
-  #--------------------------
+  # make knots
+  n_interior_vec <- 0:10
+  
+  # Build interior knots at equally spaced quantile positions of X
+  make_knots <- function(n_interior){
+    if(n_interior == 0) return(numeric(0))
+    probs <- seq(0, 1, length.out = n_interior + 2)[-c(1, n_interior + 2)]
+    return(probs)
+  }
+  
+  knot_list <- lapply(n_interior_vec, function(m) make_knots(m))
+  degree_vec <- 1:10
+  names(knot_list) <- paste0("m", n_interior_vec)  
+  
+  # K fold cross validation
+  K <- 5
+  ntrain <- length(Ytrain)
+  fold_id <- sample(rep(1:K, length.out = ntrain))
+  
+  # Storage for results
+  cv_results <- data.frame(
+    knots_id = character(),
+    degree   = integer(),
+    mean_poisson = numeric(),
+    sd_poisson   = numeric(),
+    mean_ks      = numeric(),
+    sd_ks        = numeric()
+  )
+  
+  # Main grid loop
+  for(kn_id in names(knot_list)){
+    #print(kn_id)
+    knots_vec <- knot_list[[kn_id]]
+    #print(knots_vec)
+    for(deg in degree_vec){
+      
+      poisson_fold <- numeric(K)
+      ks_fold      <- numeric(K)
+      
+      for(kk in 1:K){
+        idx_tr <- fold_id != kk
+        idx_va <- !idx_tr
+        
+        R_tr <- rankYLF[idx_tr]
+        Y_tr <- Ytrain[idx_tr]
+        S_tr <- Strain[idx_tr]   
+        R_va <- rankYLF[idx_va]
+        Y_va <- Ytrain[idx_va]
+        S_va <- Strain[idx_va]
+        
+        # Fit
+        fit_k <- fit_ispline_model_Poisson(
+          R_tr, Y_tr,
+          knots = knots_vec,
+          degree = deg
+        )
+        
+        # Predict
+        va_pred <- ispline_predict(
+          R_va, fit_k$coeff,
+          knots = knots_vec,
+          degree = deg
+        )
+        
+        poisson_fold[kk] <- poissonloss(Y_va, exp(va_pred))
+        ks_fold[kk]      <- f_ks(va_pred, S_va)
+      }
+      
+      cv_results <- rbind(cv_results,
+                          data.frame(
+                            knots_id = kn_id,
+                            degree = deg,
+                            mean_poisson = mean(poisson_fold),
+                            sd_poisson   = sd(poisson_fold),
+                            mean_ks      = mean(ks_fold),
+                            sd_ks        = sd(ks_fold)
+                          ))
+    }
+  }
+  
+  
+  
+  ks_threshold = sort( cv_results$mean_ks)[floor(length(cv_results$mean_ks)*0.1)]
+  cv_results = cv_results[which(cv_results$mean_ks<=ks_threshold ), ]
+  
+  
+  cv_results <- cv_results[order(cv_results$mean_poisson), ]
+  best_row <- cv_results[1, ]
+  
+  
+  best_knots <- knot_list[[ best_row$knots_id ]]
+  best_degree <- best_row$degree
+  print(best_knots)
+  print(best_degree)
+  
   
   
   # Fit the Ispline model on training data
   ispline_fit <- fit_ispline_model_Poisson(rankYLF, Ytrain, 
-                                   knots = c(0.25, 0.5, 0.75), degree = 2)  
+                                           knots = best_knots, degree = best_degree)  
   train_pred <- ispline_predict(rankYLF, ispline_fit$coeff, 
-                                knots = c(0.25, 0.5, 0.75), degree = 2)  
+                                knots = best_knots, degree = best_degree)  
   PRI_PL_L[k] <- poissonloss(Ytrain, exp(train_pred)) 
   PRI_KS_L[k] <- f_ks(train_pred, Strain) 
   
   
   test_pred <- ispline_predict(rankYTF, ispline_fit$coeff, 
-                               knots = c(0.25, 0.5, 0.75), degree = 2)
+                               knots = best_knots, degree = best_degree)
   PRI_PL_T[k] <- poissonloss(Ytest, exp(test_pred))
   PRI_KS_T[k] <- f_ks(test_pred, Stest) 
   
-  #--------------------------
+
   # PAVA Method
-  #--------------------------
   pava_fit <- fit_pava_model_Poisson(rankYLF, Ytrain,  solverinput = poisson_solver ) 
   gpava_pred_train <- pava_predict(pava_fit, rankYLF) 
   PRPA_PL_L[k] <- poissonloss(Ytrain, gpava_pred_train) 
   PRPA_KS_L[k] <- f_ks(gpava_pred_train, Strain)
   
-  # For test predictions, use our wrapper pava_predict:
+  # Test predictions:
   test_pred_pava <- pava_predict(pava_fit, rankYTF) 
   PRPA_PL_T[k] <- poissonloss(Ytest, test_pred_pava) 
   PRPA_KS_T[k] <- f_ks(test_pred_pava, Stest) 
 }
 
-#--------------------------
-# Summarize Results
-#--------------------------
-cat("Means:\n")
-cat("PR_PL_T:", mean(PR_PL_T), "\n")
-cat("PR_KS_T:", mean(PR_KS_T), "\n")
-cat("PR_PL_L:", mean(PR_PL_L), "\n")
-cat("PR_KS_L:", mean(PR_KS_L), "\n")
-cat("PRI_PL_T:", mean(PRI_PL_T), "\n")
-cat("PRI_KS_T:", mean(PRI_KS_T), "\n")
-cat("PRI_PL_L:", mean(PRI_PL_L), "\n")
-cat("PRI_KS_L:", mean(PRI_KS_L), "\n")
-cat("ORPA_PL_T:", mean(PRPA_PL_T), "\n")
-cat("PRPA_KS_T:", mean(PRPA_KS_T), "\n")
-cat("PRPA_PL_L:", mean(PRPA_PL_L), "\n")
-cat("PRPA_KS_L:", mean(PRPA_KS_L), "\n")
 
-cat("\nStandard Errors:\n")
-cat("PR_PL_T:", sd(PR_PL_T)/sqrt(n_iter), "\n")
-cat("PR_KS_T:", sd(PR_KS_T)/sqrt(n_iter), "\n")
-cat("PR_PL_L:", sd(PR_PL_L)/sqrt(n_iter), "\n")
-cat("PR_KS_L:", sd(PR_KS_L)/sqrt(n_iter), "\n")
-cat("PRI_PL_T:", sd(PRI_PL_T)/sqrt(n_iter), "\n")
-cat("PRI_KS_T:", sd(PRI_KS_T)/sqrt(n_iter), "\n")
-cat("PRI_PL_L:", sd(PRI_PL_L)/sqrt(n_iter), "\n")
-cat("PRI_KS_L:", sd(PRI_KS_L)/sqrt(n_iter), "\n")
-cat("PRPA_PL_T:", sd(PRPA_PL_T)/sqrt(n_iter), "\n")
-cat("PRPA_KS_T:", sd(PRPA_KS_T)/sqrt(n_iter), "\n")
-cat("PRPA_PL_L:", sd(PRPA_PL_L)/sqrt(n_iter), "\n")
-cat("PRPA_KS_L:", sd(PRPA_KS_L)/sqrt(n_iter), "\n")
+# Results of each iteration
+
+cat("PR_PL_T:", PR_PL_T, "\n")
+cat("PR_KS_T:", PR_KS_T, "\n")
+cat("PR_PL_L:", PR_PL_L, "\n")
+cat("PR_KS_L:", PR_KS_L, "\n")
+cat("PRI_PL_T:", PRI_PL_T, "\n")
+cat("PRI_KS_T:", PRI_KS_T, "\n")
+cat("PRI_PL_L:", PRI_PL_L, "\n")
+cat("PRI_KS_L:", PRI_KS_L, "\n")
+cat("PRPA_PL_T:", PRPA_PL_T, "\n")
+cat("PRPA_KS_T:", PRPA_KS_T, "\n")
+cat("PRPA_PL_L:", PRPA_PL_L, "\n")
+cat("PRPA_KS_L:", PRPA_KS_L, "\n")
